f399a23cdf250531c41f273f054754d389dd0d06
ThameeraFernando <sithnuwan111@gmail.com>
2022-12-02 08:25:32 +0530

    initial commit

---
+++ block.js
@@ -0,0 +1,18 @@
+//block class
+class Block {
+  constructor({ timestamp, lastHash, hash, data }) {
+    this.timestamp = timestamp;
+    this.lastHash = lastHash;
+    this.hash = hash;
+    this.data = data;
+  }
+}
+
+const block1 = new Block({
+  timestamp: "01/01/01",
+  lastHash: "foo-last-hash",
+  hash: "foo-hash",
+  data: "foo-data",
+});
+
+console.log("block-one", block1);
