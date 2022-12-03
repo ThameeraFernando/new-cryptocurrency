03916647d71ec5b99a6ff9a40d5024f558230292
ThameeraFernando <sithnuwan111@gmail.com>
2022-12-03 06:34:32 +0530

    adjustDifficulty using mine rate

--- block.js
+++ block.js
@@ -1,4 +1,4 @@
-const { GENESIS_DATA } = require("./config");
+const { GENESIS_DATA, MINE_RATE } = require("./config");
 const cryptoHash = require("./crypto-hash");
 
 //block class
@@ -20,11 +20,15 @@ class Block {
   static minedBlock({ lastBlock, data }) {
     let hash, timestamp;
     const lastHash = lastBlock.hash;
-    const { difficulty } = lastBlock;
+    let { difficulty } = lastBlock;
     let nonce = 0;
     do {
       nonce++;
       timestamp = Date.now();
+      difficulty = Block.adjustDifficulty({
+        originalBlock: lastBlock,
+        timestamp,
+      });
       hash = cryptoHash(timestamp, lastBlock.hash, data, difficulty, nonce);
     } while (hash.substring(0, difficulty) !== "0".repeat(difficulty));
 
@@ -36,6 +40,13 @@ class Block {
       nonce,
       hash,
     });
+  }
+  static adjustDifficulty({ timestamp, originalBlock }) {
+    const { difficulty } = originalBlock;
+    if (difficulty < 0) return 1;
+    const difference = timestamp - originalBlock.timestamp;
+    if (difference > MINE_RATE) return difficulty - 1;
+    return difficulty + 1;
   }
 }
 
