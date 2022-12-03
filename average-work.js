4a1502163181dbc8940dc5e458b4f13b916c8877
ThameeraFernando <sithnuwan111@gmail.com>
2022-12-03 07:05:28 +0530

    create a script to mesure the average work

---
+++ average-work.js
@@ -0,0 +1,28 @@
+const BlockChain = require("./blockchain");
+const blockchain = new BlockChain();
+blockchain.addBlock({ data: "initial" });
+
+let prevTimestamp, nextTimestamp, nextBlock, timeDiff, average;
+
+const times = [];
+
+for (let i = 0; i < 10000; i++) {
+  //get the previous timestamp before adding a new block
+  prevTimestamp = blockchain.chain[blockchain.chain.length - 1].timestamp;
+
+  //add a new block
+  blockchain.addBlock({ data: `block ${i}` });
+
+  //get the next timestamp after adding a new block
+  nextBlock = blockchain.chain[blockchain.chain.length - 1];
+  nextTimestamp = nextBlock.timestamp;
+
+  //get the time difference
+  timeDiff = nextTimestamp - prevTimestamp;
+
+  times.push(timeDiff);
+  average = times.reduce((total, num) => total + num) / times.length;
+  console.log(
+    `Time to mine block:${timeDiff}ms. Difficulty: ${nextBlock.difficulty}. Average time: ${average}ms`
+  );
+}
